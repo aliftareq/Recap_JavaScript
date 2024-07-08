5b202473d0f7b1b975736f37bbd73e86fb365432
alif <aliftareq@gmail.com>
2024-07-08 08:42:24 +0600

    practice problem solution

---
+++ module16/practice_problem.js
@@ -0,0 +1,38 @@
+//problem-1
+
+var harryMomgiven = 1000;
+var priceOfOrngeAndApple = 700;
+
+var shopkeeperReturn = harryMomgiven - priceOfOrngeAndApple
+
+console.log(shopkeeperReturn);
+
+
+//problem-2
+
+var Mathmetics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+var total = Mathmetics + Biology + Chemistry + Physics + Bangla;
+var avarage = total / 5
+
+console.log(avarage.toFixed(2));
+
+
+//problem-3
+
+var line_1 = "I going to be";
+var line_2 = "an awesome web developer";
+var full_line = line_1 + " " + line_2;
+console.log(full_line);
+
+
+//problem-4
+
+var givenNumber = 119;
+var remainder = 119 % 5;
+
+console.log('remainder is', remainder);
