72b0d71bb77c5c1f587712de3030262eec87e60f
alif <aliftareq@gmail.com>
2024-07-08 07:17:15 +0600

    js naming convention

---
+++ variable-name.js
@@ -0,0 +1,27 @@
+/*** naming convention ***/
+
+//1. keywords cant be used as variable
+
+// var false = 96;
+// var return = true;
+
+
+//2. cant use multiple word as variable or cant use space
+// var my home address = "chittagong"
+
+//3. variable name cant not have quotation
+//var "name" = "tom hanks"
+
+
+//4. variable cant be start with number(but cant ends with number)
+// var 1Club = "holiday"
+// var Club1 = "holiday"
+
+//5. How to use long name( with_underScore,camelCase, PascalCase)
+var my_Name = "alif";
+var myName = "alif"; //mostlyUsed
+var MyName = "Alif"
+
+//6. variable name is Case sensitive
+var person = 5;
+var Person = 5;
--- variable.js
+++ variable.js
@@ -34,3 +34,6 @@ var isFullMarks = false;
 
 
 
+
+
+
