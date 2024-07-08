0b20028065beb28007e70652c13d79f1eed87a0f
alif <aliftareq@gmail.com>
2024-07-08 06:40:29 +0600

    math for programming

---
+++ math.js
@@ -0,0 +1,17 @@
+/***
+ * ***Math for programming***
+ * number: 55, 898, -32
+ * integer, odd, even: 2,5
+ * float: decimal(2.5, 13.67,32.69)
+ * operation : addition, substruction, multiplication, division, remainder
+ * percent
+ * avarage
+ * comparison: >,<,==,!=,>=,<=
+ * unitary method
+ * max, min, avg, sum
+ * series
+ * :::Extra:::
+ * finding prime number
+ * permutation-combination
+ * area.
+ */
