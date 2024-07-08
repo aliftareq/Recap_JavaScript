1d638373da10987931b509c4ad951180e838d710
alif <aliftareq@gmail.com>
2024-07-08 07:02:28 +0600

    introduction of variable types

--- variable.js
+++ variable.js
@@ -1,5 +1,6 @@
 //vary+able = variable (things that can varie)
 
+//numeric
 var price = 11;
 var age = 24;
 var temparature = 32;
@@ -10,4 +11,26 @@ var weight = 60;
 var money = 30000;
 var job = 0;
 console.log(weight, money, job)
+
+//---------------variable types --------------------//
+
+/**
+ * 1. Numeric(int, float)
+ * 2. String
+ * 3. Boolean
+ * 
+ */
+
+//string
+var name = "Alif hossain";
+var location = "anderkilla, chittagong";
+var special = 'alia bhatt';
+
+console.log(name, location, special);
+
+//boolen(0=false, 1=true)
+var isSerious = true;
+var isFullMarks = false;
+
+
 
