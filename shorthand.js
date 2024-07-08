2d71ba02f83d2de5670fcc9666142c66a9bd200b
alif <aliftareq@gmail.com>
2024-07-08 07:39:49 +0600

    intro with mathmatical shorthand operator

---
+++ shorthand.js
@@ -0,0 +1,22 @@
+//variable declaration
+var tomatoPrice = 25;
+
+//updated variable
+var tomatoPrice = 30;
+
+//add something to previous variable
+var price1 = 31;
+var price2 = 10;
+
+//add value
+price1 = price1 + 10;
+
+//alternate(shorthand)
+price1 += 10;
+console.log(price1);
+price1 -= 10;
+console.log(price1);
+price1 *= 10;
+console.log(price1);
+price1 /= 10;
+console.log(price1);
