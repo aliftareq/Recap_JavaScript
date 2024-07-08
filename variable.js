5a705d9fdc9871a6d0e18b84011176d7c822a521
alif <aliftareq@gmail.com>
2024-07-08 06:52:39 +0600

    intro with variable

---
+++ variable.js
@@ -0,0 +1,13 @@
+//vary+able = variable (things that can varie)
+
+var price = 11;
+var age = 24;
+var temparature = 32;
+console.log(price);
+
+//---task
+var weight = 60;
+var money = 30000;
+var job = 0;
+console.log(weight, money, job)
+
