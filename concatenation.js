1d5a43838d3f073f28172be4b241553ab85a921e
alif <aliftareq@gmail.com>
2024-07-08 07:54:56 +0600

    concatenation & parseInt parseFloat

---
+++ concatenation.js
@@ -0,0 +1,31 @@
+//string concatetion, parseInt , parseFloat
+
+
+//string concatetion
+var firstName = "kim";
+var lastName = "kardasian";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+//addition string with/as number
+
+var price1 = 31;
+var price2 = "10";
+var total_1 = price1 + price2;
+console.log(total_1);
+
+var price3 = "31";
+var price4 = "10";
+var total_2 = price3 + price4;
+
+console.log(total_2);
+
+
+
+//parseFloat, parseInt (type conversion)
+var price5 = parseInt("31");
+var price6 = parseInt("10");
+var total_3 = price5 + price6;
+
+console.log("total_3", total_3);
