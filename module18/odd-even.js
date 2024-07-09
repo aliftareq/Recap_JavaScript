5190c8625909b89db0e8b3c5f2033d57a5517cc6
alif <aliftareq@gmail.com>
2024-07-09 07:59:54 +0600

    intro with while loop & finding odd-even numbers

---
+++ module18/odd-even.js
@@ -0,0 +1,33 @@
+//display 1 to 10;
+
+// var number = 1;
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
+
+//display even number between 1 to 10
+
+// var number = 0
+// while (number < 10) {
+//     console.log(number);
+//     number = number + 2
+// }
+
+//even
+// var number = 0;
+// while (number < 20) {
+//     if (number % 2 == 0) {
+//         console.log(number);
+//     }
+//     number++
+// }
+
+//odd
+var number = 0;
+while (number < 20) {
+    if (number % 2 != 0) {
+        console.log(number);
+    }
+    number++
+}
---
+++ module18/while.js
@@ -0,0 +1,18 @@
+//loop practices (while , for)
+
+/* **structure**
+
+while (conndition) {
+    body
+    increment
+
+}*/
+
+//example
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log(roastGiven, 'please, roast den!');
+    // roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    roastGiven++
+}
