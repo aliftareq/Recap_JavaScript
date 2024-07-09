6336549dfe34888392ac69d7c58d51ddf487de51
alif <aliftareq@gmail.com>
2024-07-09 16:36:11 +0600

    recap switch case

---
+++ module19/switch.js
@@ -0,0 +1,53 @@
+//use of case 'switch'
+
+
+//use of multiple condition through if-else
+var color = 'yellow';
+
+if (color == 'green') {
+    console.log('your are my green color friend');
+}
+else if (color == 'blue') {
+    console.log('your are my blue color friend');
+}
+else if (color == 'red') {
+    console.log('your are my red color friend');
+}
+else if (color == 'white') {
+    console.log('your are my white color friend');
+}
+else if (color == 'yellow') {
+    console.log('your are my yellow color himu friend');
+}
+else {
+    console.log('your are my cream color friend');
+}
+
+
+//alternate: use of multiple condition by 'switch'
+
+
+switch (color) {
+    case 'green':
+        console.log('your are my green color friend');
+        break;
+    case 'blue':
+        console.log('your are my blue color friend');
+        break;
+    case 'red':
+        console.log('your are my red color friend');
+        break;
+    case 'white':
+        console.log('your are my white color friend');
+        break;
+    case 'yellow':
+        console.log('your are my yellow color friend');
+        break;
+    default:
+        console.log('your are my cream color friend');
+}
+
+
+
+
+
